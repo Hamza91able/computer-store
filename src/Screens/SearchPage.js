46f46811d34226ab0a14ba8dce4a249844caf794
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2020-01-15 19:22:58 +0500

    Added search api

--- src/Screens/SearchPage.js
+++ src/Screens/SearchPage.js
@@ -3,6 +3,9 @@ import { withStyles } from '@material-ui/core/styles';
 import { Container, AppBar, Toolbar, Typography, Paper, Grid, Card, CardContent, Divider, Box } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
+
+import connectionString from '../Static/Utilities/connectionString';
 
 const styles = theme => ({
     rating1: {
@@ -20,10 +23,49 @@ const styles = theme => ({
     }
 });
 
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'PKR',
+    minimumFractionDigits: 2
+})
+
 class SearchPage extends Component {
+
+    state = {
+        products: [],
+    }
+
+    componentDidMount() {
+        this.searchProduct();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.match.params.k !== prevProps.match.params.k) {
+            this.searchProduct();
+        }
+    }
+
+    searchProduct = () => {
+        const keyWord = this.props.match.params.k;
+
+        Axios({
+            url: `${connectionString}/products/get-products-by-keyword/${keyWord}`,
+            method: 'GET',
+        }).then(res => {
+            console.log(res.data.products);
+            this.setState({
+                products: res.data.products,
+            })
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+
 
     render() {
         const { classes } = this.props;
+        const { products } = this.state;
 
         return (
             <div>
@@ -39,96 +81,62 @@ class SearchPage extends Component {
                         position='relative'
                     >
                         <Toolbar>
-                            <Typography variant='caption'>
-                                1-16 of 29 results for <strong style={{ color: '#c45500' }}>"{this.props.match.params.k}"</strong>
+                            <Typography variant='body1'>
+                                1-{products.length} of {products.length} results for <strong style={{ color: '#c45500' }}>"{this.props.match.params.k}"</strong>
                             </Typography>
                         </Toolbar>
                     </AppBar>
                     <br />
-                    <Grid container>
-                        <Grid item xs={12} md={3}>
-                            <div style={{
-                                justifyContent: 'center',
-                                alignItems: 'center,',
-                                display: 'inline-flex',
-                                width: '100%',
-                            }}>
-                                <Link to='/product-details'>
-                                    <img src="https://m.media-amazon.com/images/I/71RsweT83eL._AC_UY218_ML3_.jpg" />
-                                </Link>
-                            </div>
+                    {products.length > 0 ? products.map((product, value) => {
+                        return (
+                            <React.Fragment>
+                                <Grid container>
+                                    <Grid item xs={12} md={3}>
+                                        <div style={{
+                                            justifyContent: 'center',
+                                            alignItems: 'center,',
+                                            display: 'inline-flex',
+                                            width: '100%',
+                                        }}>
+                                            {product.pictures &&
+                                                <Link to={`/product-details/${product._id}`}>
+                                                    <img style={{ height: 218, width: 242 }} src={product.pictures[0]} />
+                                                </Link>}
+                                        </div>
 
-                        </Grid>
-                        <Grid item xs={12} md={9}>
-                            <Link className={classes.link} to='/product-details'>
-                                <Typography style={{ fontWeight: 'bold' }} className={classes.title} gutterBottom>
-                                    Intel Core i9-9900K Desktop Processor 8 Cores up to 5.0 GHz Turbo unlocked LGA1151 300 Series 95W
-                            </Typography>
-                            </Link>
-                            <div className={classes.rating1}>
-                                <Rating
-                                    name="hover-side"
-                                    value={5}
-                                    size='small'
-                                />
-                                <Box style={{ marginTop: -7, fontSize: 13 }} ml={1}><Link to='/product-details'>680</Link></Box>
-                            </div>
-                            <Link>
-                                <Typography variant='caption' style={{ fontSize: 14, fontWeight: 'bold' }}>
-                                    Processors
-                                </Typography>
-                            </Link>
-                            <br />
-                            <Typography variant='caption' style={{ fontSize: 14, fontWeight: 'bold' }}>
-                                Rs. 76,641
-                            </Typography>
-                            <Typography style={{ fontSize: 13 }} className={classes.title} color="textSecondary" gutterBottom>
-                                Sold and Shipped by: <strong>Computer Store</strong>
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                    <Divider style={{ marginTop: 20, marginBottom: 20 }} />
-                    <Grid container>
-                        <Grid item xs={12} md={3}>
-                            <div style={{
-                                justifyContent: 'center',
-                                alignItems: 'center,',
-                                display: 'inline-flex',
-                                width: '100%',
-                            }}>
-                                <Link to='/product-details'>
-                                    <img src="https://m.media-amazon.com/images/I/81t4McdI7PL._AC_UY218_ML3_.jpg" />
-                                </Link>
-                            </div>
-                        </Grid>
-                        <Grid item xs={12} md={9}>
-                            <Link className={classes.link} to='/product-details'>
-                                <Typography style={{ fontWeight: 'bold' }} className={classes.title} gutterBottom>
-                                    Intel Core i9-9820X X-Series Processor 10 Cores up to 4.1GHz Turbo Unlocked LGA2066 X299 Series 165W
-                                </Typography>
-                            </Link>
-                            <div className={classes.rating1}>
-                                <Rating
-                                    name="hover-side"
-                                    value={4}
-                                    size='small'
-                                />
-                                <Box style={{ marginTop: -7, fontSize: 13 }} ml={1}><Link to='/product-details'>10</Link></Box>
-                            </div>
-                            <Link>
-                                <Typography variant='caption' style={{ fontSize: 14, fontWeight: 'bold' }}>
-                                    Processors
-                                </Typography>
-                            </Link>
-                            <br />
-                            <Typography variant='caption' style={{ fontSize: 14, fontWeight: 'bold' }}>
-                                Rs. 92,970
-                            </Typography>
-                            <Typography style={{ fontSize: 13 }} className={classes.title} color="textSecondary" gutterBottom>
-                                Sold and Shipped by: <strong>Computer Store</strong>
-                            </Typography>
-                        </Grid>
-                    </Grid>
+                                    </Grid>
+                                    <Grid item xs={12} md={9}>
+                                        <Link className={classes.link} to={`/product-details/${product._id}`}>
+                                            <Typography style={{ fontWeight: 'bold' }} className={classes.title} gutterBottom>
+                                                {product.title}
+                                            </Typography>
+                                        </Link>
+                                        <div className={classes.rating1}>
+                                            <Rating
+                                                name="hover-side"
+                                                value={product.rating}
+                                                size='small'
+                                            />
+                                            <Box style={{ marginTop: -7, fontSize: 13 }} ml={1}><Link to={`/product-details/${product._id}`}>{product.rating ? product.rating.length : "0"}</Link></Box>
+                                        </div>
+                                        <Link>
+                                            <Typography variant='caption' style={{ fontSize: 14, fontWeight: 'bold' }}>
+                                                {product.category}
+                                            </Typography>
+                                        </Link>
+                                        <br />
+                                        <Typography variant='caption' style={{ fontSize: 14, fontWeight: 'bold' }}>
+                                            {formatter.format(product.price)}
+                                        </Typography>
+                                        <Typography style={{ fontSize: 13 }} className={classes.title} color="textSecondary" gutterBottom>
+                                            Sold and Shipped by: <strong>{product.soldAndShippedBy}</strong>
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                                <Divider style={{ marginTop: 20, marginBottom: 20 }} />
+                            </React.Fragment>
+                        )
+                    }) : <h1>Loading</h1>}
                 </Container>
             </div>
         );
