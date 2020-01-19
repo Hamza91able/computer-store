5cc76c6a2ad035753f67d2489678b871cccd465b
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2020-01-19 07:19:49 +0500

    Added hosting api

--- src/Static/Utilities/connectionString.js
+++ src/Static/Utilities/connectionString.js
@@ -1,3 +1,4 @@
-const connectionString = "http://localhost:8080";
+// const connectionString = "http://localhost:8080";
+const connectionString = "https://computer-store-app.herokuapp.com"
 
 module.exports = connectionString;
