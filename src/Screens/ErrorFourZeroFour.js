2cfe0ec1924d1539a4180fcc40c846a7a592d974
Muhammad Hamza Khan <m.hamzakhan91able@gmail.com>
2020-01-23 05:23:45 +0500

    Fixed 404 page on mobile

--- src/Screens/ErrorFourZeroFour.js
+++ src/Screens/ErrorFourZeroFour.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import { Container, Grid, Typography, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+
+const styles = theme => ({
+    page: {
+        fontSize: '20px',
+        lineHeight: '25pxpx',
+        marginBottom: '10px',
+        [theme.breakpoints.up('md')]: {
+            fontSize: '80px',
+            lineHeight: '90px',
+            marginBottom: '30px'
+        }
+    }
+});
 
 class ErrorFourZeroFour extends Component {
 
     render() {
+        const { classes } = this.props;
 
         return (
             <div style={{ backgroundColor: 'black', height: '80vh', color: 'white' }}>
                 <Container style={{ paddingTop: 50 }} maxWidth='lg'>
                     <Grid container>
                         <Grid item xs={9}>
-                            <Typography style={{ fontSize: '80px', lineHeight: '90px', marginBottom: '30px' }}>
+                            <Typography className={classes.page}>
                                 Our apologies.
                                 <br />
                                 We're unable to find the
@@ -42,4 +57,4 @@ class ErrorFourZeroFour extends Component {
     }
 }
 
-export default ErrorFourZeroFour;
+export default withStyles(styles)(ErrorFourZeroFour);
